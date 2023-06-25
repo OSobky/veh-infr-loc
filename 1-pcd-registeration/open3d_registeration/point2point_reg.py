c3dc0005ae11f2c7befe3aac20030fe8aea73d3c
osobky <omarelsobky97@gmail.com>
2023-06-25 15:05:14 +0200

    adding utils folder and scripts

--- 1-pcd-registeration/open3d_registeration/point2point_reg.py
+++ 1-pcd-registeration/open3d_registeration/point2point_reg.py
@@ -4,27 +4,61 @@ import os
 import numpy as np
 import open3d as o3d
 
-from utils import draw_registration_result
+from utils import (draw_registration_result, execute_global_registration,
+                   prepare_dataset)
 
 
-def point2point_reg(src_dir, trgt_dir):
+def point2point_reg(src_dir, trgt_dir, voxel_size):
 
-
-    for src_pcd_filename, trgt_pcd_filename in zip(
-        sorted(os.listdir(src_dir)), sorted(os.listdir(trgt_dir))):
+    trans_init_dir = os.path.join(src_dir, "../o3d_veh_infra_global_reg_transf/")
+    
+    for src_pcd_filename, trgt_pcd_filename, trans_init_filename in zip(
+        sorted(os.listdir(src_dir)), sorted(os.listdir(trgt_dir)), 
+        sorted(os.listdir(trans_init_dir))):
 
         print("src pcd filename: ", src_pcd_filename)
         print("trgt pcd filename: ", trgt_pcd_filename)
-        trans_init = np.asarray([[1, 0, 0, 0],
-                         [0, 1, 0, 0],
-                         [0, 0, 0, 1], [0.0, 0.0, 0.0, 1.0]])
-
-        source = o3d.io.read_point_cloud(os.path.join(src_dir,src_pcd_filename))
-        target = o3d.io.read_point_cloud(os.path.join(trgt_dir,trgt_pcd_filename))
-        draw_registration_result(source, 
-                                target, 
+        print("trans init filename: ", trans_init_filename)
+        trans_init = np.load(os.path.join(trans_init_dir,trans_init_filename))
+        threshold = 0.02
+        # source = o3d.io.read_point_cloud(os.path.join(src_dir,src_pcd_filename))
+        # target = o3d.io.read_point_cloud(os.path.join(trgt_dir,trgt_pcd_filename))
+        source, target, source_down,\
+        target_down, source_fpfh, target_fpfh = prepare_dataset(os.path.join(src_dir,src_pcd_filename), 
+                                                                os.path.join(trgt_dir,trgt_pcd_filename),
+                                                                voxel_size)
+        draw_registration_result(source_down, 
+                                target_down, 
                                 trans_init)
 
+        print("Apply point-to-point ICP")
+        reg_p2p = o3d.pipelines.registration.registration_icp(
+            source_down, target_down, threshold, trans_init,
+            o3d.pipelines.registration.TransformationEstimationPointToPoint())
+        print(reg_p2p)
+        print("Transformation is:")
+        print(reg_p2p.transformation)
+        draw_registration_result(source_down, target_down, reg_p2p.transformation)
+        break
+
+def global_reg(src_dir, trgt_dir, voxel_size):
+
+    for src_pcd_filename, trgt_pcd_filename in zip(
+        sorted(os.listdir(src_dir)), sorted(os.listdir(trgt_dir))):
+        source, target, source_down,\
+        target_down, source_fpfh, target_fpfh = prepare_dataset(os.path.join(src_dir,src_pcd_filename), 
+                                                                os.path.join(trgt_dir,trgt_pcd_filename),
+                                                                voxel_size)
+        result_ransac = execute_global_registration(source_down, target_down,
+                                            source_fpfh, target_fpfh,
+                                            voxel_size)
+        print(result_ransac)
+        print(type(result_ransac.transformation))
+        draw_registration_result(source_down, target_down, result_ransac.transformation)
+        np.save(os.path.join(src_dir, "../o3d_veh_infra_global_reg_transf/", os.path.splitext(src_pcd_filename)[0]), 
+                result_ransac.transformation)
+
+        break
 
 
 if __name__ == '__main__':
@@ -34,4 +68,6 @@ if __name__ == '__main__':
     parser.add_argument('--output-dir', type=str, required=True, help='Path to output transformation matrices for each point cloud in kitti format')
     args = parser.parse_args()
 
-    point2point_reg(args.src_dir, args.trgt_dir)
+    voxel_size = 0.05
+    global_reg(args.src_dir, args.trgt_dir, voxel_size)  # means 5cm for this dataset
+    point2point_reg(args.src_dir, args.trgt_dir, voxel_size )
--- 1-pcd-registeration/open3d_registeration/utils.py
+++ 1-pcd-registeration/open3d_registeration/utils.py
@@ -1,5 +1,6 @@
 import copy
 
+import numpy as np
 import open3d as o3d
 
 
@@ -14,3 +15,57 @@ def draw_registration_result(source, target, transformation):
                                       front=[0.9288, -0.2951, -0.2242],
                                       lookat=[1.6784, 2.0612, 1.4451],
                                       up=[-0.3402, -0.9189, -0.1996])
+# For Global registeration
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+# downsample the the dataset for global regsiteration
+def prepare_dataset(src_dir, trgt_dir, voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+
+
+    source = o3d.io.read_point_cloud(src_dir)
+    target = o3d.io.read_point_cloud(trgt_dir)
+    trans_init = np.asarray([[1, 0, 0, 0],
+                         [0, 1, 0, 0],
+                         [0, 0, 0, 1], [0.0, 0.0, 0.0, 1.0]])
+
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+# execute the global registeration 
+def execute_global_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, True,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    return result
+
